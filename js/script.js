3b25be70be31c3fcfe06d0c586ae8e526ac3e37f
Caio <caiowgcw@gmail.com>
2021-02-01 22:04:15 -0300

    Começo

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const url = 'https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo';
+const masculino = Symbol('M');
+const feminino = Symbol('W');
+let mediaIdade = 0;
+let somaIdade = 0;
+
+
+window.addEventListener('load', () => {
+
+});
+
+
+const data
